398816e84e949346137a8c4fc2ee375b754bb2fa
Sl00g <jadenrb77@gmail.com>
2024-09-05 12:53:49 -0400

    added retrieval of stock name

--- stock-scraper.py
+++ stock-scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 start_url = 'https://www.centralcharts.com/'
 pages = []
-for i in range(5):
+for i in range(1,2):
     end_url = 'en/price-list-ranking/ALL/desc/ts_19-us-nasdaq-stocks--qc_3-previous-close-change?p='
     #this end_url is interchangable with other stock markets; this specific one is related to NASDAQ
     new_page = start_url+end_url+str(i)
@@ -14,3 +14,11 @@ stocks = [] #looking for: name, price, percent change from previous, volume (i.e
 for page in pages:
     page_html = requests.get(page).text
     soup = bs(page_html, 'lxml') #lxml is our specific parser in this occassion; works with broken html
+    table = soup.find('table', class_ = 'tabMini tabQuotes')
+    stocks_html = table.find_all('tr')
+    for stock in stocks_html:
+        stock_name = stock.find('a', class_ = 'tooltip-img')
+        if(stock_name!=None):
+            stock_name = stock_name.text
+            print(stock_name)
+    
