f2de1b85f2d52dccdd43c64c82a64b1d7cd4140c
Sl00g <jadenrb77@gmail.com>
2024-09-04 22:41:24 -0400

    started for each loop to cycle through pages

--- stock-scraper.py
+++ stock-scraper.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 start_url = 'https://www.centralcharts.com/'
 pages = []
-for page in range(5):
+for i in range(5):
     end_url = 'en/price-list-ranking/ALL/desc/ts_19-us-nasdaq-stocks--qc_3-previous-close-change?p='
-    new_page = start_url+end_url+str(page)
+    #this end_url is interchangable with other stock markets; this specific one is related to NASDAQ
+    new_page = start_url+end_url+str(i)
     pages.append(new_page)
 
-
-
+stocks = [] #looking for: name, price, percent change from previous, volume (i.e. total # shares)
+for page in pages:
+    page_html = requests.get(page).text
+    soup = bs(page_html, 'lxml') #lxml is our specific parser in this occassion; works with broken html
