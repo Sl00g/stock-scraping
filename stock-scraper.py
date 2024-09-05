09f680a088be89a031afc56bed7725550c5d1c3a
Sl00g <jadenrb77@gmail.com>
2024-09-04 21:59:59 -0400

    created list of page urls

--- stock-scraper.py
+++ stock-scraper.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+
+start_url = 'https://www.centralcharts.com/'
+pages = []
+for page in range(5):
+    end_url = 'en/price-list-ranking/ALL/desc/ts_19-us-nasdaq-stocks--qc_3-previous-close-change?p='
+    new_page = start_url+end_url+str(page)
+    pages.append(new_page)
+
+
+
