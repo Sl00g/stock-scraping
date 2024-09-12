51545d3849d6c5a535c6f0039005ab913ddb7a60
Sl00g <jadenrb77@gmail.com>
2024-09-11 22:05:08 -0400

    added user input for cash, sorting dataframe based on num of shares, automated scraping to be hourly

--- stock-scraper.py
+++ stock-scraper.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import time
 
-def makePages(): #can modify this later so that you are using a user inputted url
+def makePages():
     start_url = 'https://www.centralcharts.com/'
     pages = []
-    for i in range(1,2):
-        end_url = 'en/price-list-ranking/ALL/desc/ts_19-us-nasdaq-stocks--qc_3-previous-close-change?p='
-        #this end_url is interchangable with other stock markets; this specific one is related to NASDAQ
+    for i in range(1,5):
+        end_url = 'en/price-list-ranking/ALL/desc/ts_20-us-tech-150--qc_2-daily-change?p='
+        #this end_url is interchangable with other stock markets; this specific one is related to US' Tech150
         new_page = start_url+end_url+str(i)
         pages.append(new_page)
     return pages
@@ -15,34 +16,59 @@ def makePages(): #can modify this later so that you are using a user inputted ur
 def getListTD(stock): #creates a list of td elements corresponding to a certain stock
     return stock.find_all('td')
 
-pages = makePages()
-stocks = [] #looking for: name, price, percent change from previous, volume (i.e. total # shares)
-for page in pages:
-    page_html = requests.get(page).text
-    soup = bs(page_html, 'lxml') #lxml is our specific parser in this occassion; works with broken html
-    table = soup.find('table', class_ = 'tabMini tabQuotes').tbody #there is a tr element in the table head, we don't want that
-    stocks_html = table.find_all('tr')
-    for stock in stocks_html:
-        tdList = getListTD(stock)
-        stock_name = tdList[0].a.text
-        stock_price = tdList[1].span.text
-        stock_percent_change = tdList[2].span.text
-        stock_volume = tdList[6].span.text
-        new_stock = [stock_name, stock_price, stock_percent_change, stock_volume]
-        stocks.append(new_stock)
-#get stock link
+def scrape_data():
+    #scrape info from other sites: intrinsic value, stock abbreviation
+    pages = makePages()
+    stocks = [] #looking for: name, price, percent change from previous, volume (i.e. total # shares)
+    for page in pages:
+        page_html = requests.get(page).text
+        soup = bs(page_html, 'lxml') #lxml is our specific parser in this occassion; works with broken html
+        table = soup.find('table', class_ = 'tabMini tabQuotes').tbody #there is a tr element in the table head, we don't want that
+        stocks_html = table.find_all('tr')
+        for stock in stocks_html:
+            tdList = getListTD(stock)
+            stock_name = tdList[0].a.text
+            stock_price = (float)(tdList[1].span.text.replace(',',''))
+            stock_percent_change = tdList[2].span.text
+            stock_volume = (int)(tdList[6].span.text.replace(',', ''))
+            new_stock = [stock_name, stock_price, stock_percent_change, stock_volume]
+            stocks.append(new_stock)
+    #get stock link
 
-df = pd.DataFrame(stocks, columns=['Stock', 'Current Price', '%Change', '# of Shares'])
-df.to_csv('stocks.csv')
-print('Created csv')
+    df = pd.DataFrame(stocks, columns=['Stock', 'Current Price', '%Change', '# of Shares'])
+    #df.to_csv('stocks.csv')
+    print('Created csv')
 
-#operate on csv file via pandas
-#should be able to carry out operations like:
-#find top performing stock of the day (most percent up)
-#sort by price
-#sort alphabetically
-#get intrinsic price
+    #should be able to carry out operations like:
+    #find top performing stock of the day (most percent up)
+    #sort by price
+    #sort alphabetically
+    #get intrinsic price
+    #should introduce a watchlist; specific stocks to display
+
+    #first, you should get user input, maybe amount of cash they want to invest
+    #give them stocks that meet their cash price, include link
+    print('How much cash do you plan to invest?')
+    cash = (float)(input('$'))
+    #might want to loop until you get an actual float, figure out later
+
+    condition = df['Current Price'] <= cash
+    df_cash = df[condition] #this formats dataframe so that it only includes stocks within cash range
+
+    #print('Filter - 1. Alphabetically\n2. By Price\n3. By \%Change\n4. Number of Shares')
+
+    #should sort by number of shares so they get mainstream / large-cap companies
+    #could also use some formatting prettiness; getting rid of numbers on the side, spacing out further
+
+    df_cash = df_cash.sort_values(by="# of Shares", ascending=False)
+    print(df_cash)
+
+    #pull up the dataframe with stocks that meet their price range
+    #they should be able to search for a stock; ignore their available cash
 
 
-#first, you should get user input, maybe amount of cash they want to invest
-#give them stocks that meet their cash price, include link
+if __name__ == '__main__':
+    while True:
+        scrape_data()
+        time.sleep(3600) 
+        #scrapes hourly
